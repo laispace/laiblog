2f5d5be6cd8adb93376f809eed4b7bd9bd3b4b9e
laispace <qingsheng.lai@gmail.com>
2015-08-22 12:51:00 +0800

    update config

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blogs published URL.
-        url: 'http://localhost:2368',
+        url: 'http://127.0.0.1:2368',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
